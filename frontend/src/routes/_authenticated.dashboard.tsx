31342027db75dce62893b9a2911b3301c142e144
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-29 20:26:33 +0800

    feat: update dashboard page

--- frontend/src/routes/_authenticated.dashboard.tsx
+++ frontend/src/routes/_authenticated.dashboard.tsx
@@ -5,5 +5,29 @@ export const Route = createFileRoute('/_authenticated/dashboard')({
 })
 
 function DashboardPage() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <h1 className="text-4xl mb-12">Dashboard</h1>
+      <div className="flex gap-3">
+        <div className="p-3 border-primary border-2 rounded-md w-60">
+          <h1 className="text-md font-extrabold">TOTAL MEMBERS</h1>
+          <p>2000</p>
+        </div>
+        <div className="p-3 border-primary border-2 rounded-md w-60">
+          <h1 className="text-md font-extrabold">ACTIVE MEMBERSHIPS</h1>
+          <p>150</p>
+        </div>
+        <div className="p-3 border-primary border-2 rounded-md w-60">
+          <h1 className="text-md font-extrabold">TOTAL REVENUE</h1>
+          <p>₱12,500</p>
+        </div>
+      </div>
+      <div className="mt-12">
+        <h1>Recent members</h1>
+      </div>
+      <div className="mt-12">
+        <h1>Expiring memberships</h1>
+      </div>
+    </div>
+  )
 }
