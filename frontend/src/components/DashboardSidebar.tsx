59e5a7fd0a2eda73ad4e0d6d74dd59bfd774527c
bangueco <121280104+bangueco@users.noreply.github.com>
2025-01-01 19:32:07 +0800

    feat: update dashboard sidebar

--- frontend/src/components/DashboardSidebar.tsx
+++ frontend/src/components/DashboardSidebar.tsx
@@ -19,14 +19,14 @@ export default function DashboardSidebar() {
   }
 
   return (
-    <nav className="flex flex-col w-56 h-screen p-2 bg-secondary items-center">
+    <nav className="flex flex-col w-64 h-screen p-2 bg-secondary items-center">
       <h1 className="text-2xl font-bold text-primary">GymOps</h1>
-      <div className="flex flex-col gap-2 mt-20">
-        <Link className="text-md hover:border-primary border p-2 rounded-md" to="/dashboard">Dashboard</Link>
-        <Link className="text-md hover:border-primary border p-2 rounded-md" to="/manage-members">Manage Members</Link>
-        <Link className="text-md hover:border-primary border p-2 rounded-md" to="/dashboard">Manage Memberships</Link>
-        <Link className="text-md hover:border-primary border p-2 rounded-md" to="/dashboard">Settings</Link>
-        <button onClick={onPressLogout} className="text-md hover:border-primary border p-2 rounded-md text-left">Logout</button>
+      <div className="flex flex-col gap-2 mt-20 w-full">
+        <Link className="text-sm hover:border-primary border p-2 rounded-md" to="/dashboard">Dashboard</Link>
+        <Link className="text-sm hover:border-primary border p-2 rounded-md" to="/manage-members">Manage Members</Link>
+        <Link className="text-sm hover:border-primary border p-2 rounded-md" to="/manage-memberships">Manage Memberships</Link>
+        <Link className="text-sm hover:border-primary border p-2 rounded-md" to="/dashboard">Settings</Link>
+        <button onClick={onPressLogout} className="text-sm hover:border-primary border p-2 rounded-md text-left">Logout</button>
       </div>
     </nav>
   )
