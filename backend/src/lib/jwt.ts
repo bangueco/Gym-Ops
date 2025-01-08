cc07343b5e47ef95a88531528b6aa3e8e9856148
bangueco <121280104+bangueco@users.noreply.github.com>
2025-01-08 21:45:50 +0800

    refactor: change id to userId in jwt

--- backend/src/lib/jwt.ts
+++ backend/src/lib/jwt.ts
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 import config from "./utils/config";
 
-const generateAccessToken = (id: number, firstName: string, lastName: string, email: string): string => {
-  return jwt.sign({ id, firstName, lastName, email }, config.accessTokenKey, { expiresIn: "5m" });
+const generateAccessToken = (userId: number, firstName: string, lastName: string, email: string): string => {
+  return jwt.sign({ userId, firstName, lastName, email }, config.accessTokenKey, { expiresIn: "5m" });
 };
 
-const generateRefreshToken = (id: number, firstName: string, lastName: string, email: string): string => {
-  return jwt.sign({ id, firstName, lastName, email }, config.refreshTokenKey, { expiresIn: "30d" });
+const generateRefreshToken = (userId: number, firstName: string, lastName: string, email: string): string => {
+  return jwt.sign({ userId, firstName, lastName, email }, config.refreshTokenKey, { expiresIn: "30d" });
 };
 
 const verifyAccessToken = (token: string): string | jwt.JwtPayload => {
